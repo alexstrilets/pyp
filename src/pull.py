43e5b30ff1eee886bf7cb44ee5bc71cbc467c1ad
Alex Strilets <astrilet@gmail.com>
2015-11-19 13:47:25 -0700

    updated pull.py

--- src/pull.py
+++ src/pull.py
@@ -5,20 +5,27 @@ import datetime
 import os.path
 
 
-stocksToPull = "AAPL", "GOOG"
+stocksToPull = "AAPL","GOOG",'MSFT','CMG','AMZN','EBAY','TSLA'
 
-def getLastPullDate(stock):
-    return 0
+def getLastPullDate(fname):
+    if not os.path.isfile(fname):
+        return 0
+    else:
+        fPtr=open(fname, 'r')
+        existingData=fPtr.read().split('\n')
+        lastLine = existingData[-2]
+        fPtr.close()
+        return int(lastLine.split(',')[0])
 
 def pullData(stock):
-    print "Pulling stock " + stock
+    print "-------- Pulling stock " + stock
     print str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
     urlToPull = 'http://chartapi.finance.yahoo.com/instrument/1.0/'+stock+'/chartdata;type=quote;range=1y/csv'
     saveFileName = stock+'.txt'
+    lastDateOnFile=getLastPullDate(saveFileName)
     saveFile=open(saveFileName, 'a')
     htmlPage=urllib2.urlopen(urlToPull).read()
     splitHtml=htmlPage.split('\n')
-    lastDateOnFile=getLastPullDate(stock)
 
     for eachLine in splitHtml:
         if 'values' not in eachLine:
@@ -28,7 +35,7 @@ def pullData(stock):
                     lineToWrite=eachLine+'\n'
                     saveFile.write(lineToWrite)
     saveFile.close()
-    print "Pulled stock " + stock
+    print "------- Done ------- "
     time.sleep(1)
 
 for stock in stocksToPull:
